2f8fe12ba82b3468de1528efbe4e6676e112dcfd
prithvi <tamangprithvi20@gmai.com>
2021-08-05 20:32:29 +0530

    Understanding Primitive DataTypes

--- JavaScriptFundaPart1/app.js
+++ JavaScriptFundaPart1/app.js
@@ -6,3 +6,12 @@ let firstJob = "Programmer"
 let countryName = "India"
 let PI = 3.14;
 
+//Data types
+//There are 5 data types in js number, string, boolean, undefined, null
+
+let ageOfPrithvi = 23;//number it can be floating point number
+let firstName = "Prithvinarayan";//string is the sequence of character
+let isAbleToWalk = true;//it contains only true or false
+let dogName;//it will undefined without initialized
+
+
