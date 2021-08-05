1c0704750cae258c8672e05bdcb4328b474f3393
prithvi <tamangprithvi20@gmai.com>
2021-08-05 21:36:44 +0530

    Understanding Basic Operators

--- JavaScriptFundaPart1/app.js
+++ JavaScriptFundaPart1/app.js
@@ -1,7 +1,7 @@
 //naming convenstion
 //always use camel case when you are declaring variable and variable should be descriptive
 
-let firstName = "Prithvinarayan"
+let fullName = "Prithvinarayan"
 let firstJob = "Programmer"
 let countryName = "India"
 let PI = 3.14;
@@ -22,5 +22,43 @@ const birthYear = 1998;//const is never be reassigned in one application
 // birthYear =1999;   it will throw error
 var myAge = 23;//var is same as let but it having some older feature of js
 myAge = 24;
+
+//Understanding Basic Operators
+//Mathmetical Assigement Operators  +,-,*,/,%;
+
+const numberOfApples = 4;// if we want to add number it will give sum
+const numberOfOranges = 5;
+const totalFruites = numberOfApples + numberOfOranges;
+console.log(`Number of Apples ${numberOfApples} and Number of Oranges ${numberOfOranges} total number of fruites ${totalFruites}`)
+
+const fName = "Prithvinarayan";// But the when we working on string it will concaticant that string
+const lName = "Tamang";
+const fullNameOfPrithvi = fName + lName;
+console.log(`Full name of Prithvi ${fullNameOfPrithvi}`);
+
+const ageOfPrithviInFuture = 2037 - 1998;//It will subtracnt the number but in case of string it cant be the subtract the sting; it will throuw error
+console.log(`Age of Prithvi in 2037 ${ageOfPrithviInFuture}`)
+
+console.log(2 * 3, 2 - 3, 4 + 4, 4 / 2, 4 % 2);//all areathmetic operation will perform in single console log;
+
+//Assigenment Operator =
+let x = 10 + 5;
+//compound Assigenment Operators
+x += 10;//x = 15 + 10 = 25
+x *= 4;//x = 25 * 4 = 100
+x /= 5; //x = 100 / 5;
+x++;
+x--;
+console.log(x);
+
+//Comparision OPerators
+//Comparison Operator should be boolean True or false >----- >, <, <=, >=, ===(Stricly equal to)
+
+const heightPrithvi = 5.3;
+const heightKhali = 7.1;
+console.log(heightKhali > heightPrithvi);
+
+
+
 
 
