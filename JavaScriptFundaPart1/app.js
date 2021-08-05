c45e72463f419ab0ce9e38a076c469e1991a0b8b
prithvi <tamangprithvi20@gmai.com>
2021-08-05 20:27:08 +0530

    Naming Convenstion

---
+++ JavaScriptFundaPart1/app.js
@@ -0,0 +1,8 @@
+//naming convenstion
+//always use camel case when you are declaring variable and variable should be descriptive
+
+let firstName = "Prithvinarayan"
+let firstJob = "Programmer"
+let countryName = "India"
+let PI = 3.14;
+
